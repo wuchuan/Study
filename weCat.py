6181c77577b71ca5020d0290704dcd735712fb16
wuchuan <1012496706@qq.com>
2017-09-25 15:48:24 +0800

    完成wakeonlan启动

--- weCat.py
+++ weCat.py
@@ -1,8 +1,8 @@
+#incoding=utf8
 import itchat
 import commands
 from itchat.content import TEXT
 
-mode=normal
 #""""""
 #    node
 #        normal 正常模式
@@ -11,24 +11,38 @@ mode=normal
 #'''
 
 def mode_cmd(cmd):
-    return   commands.getstatusoutput(cmd)
+    print('cmd=='+cmd)
+    print commands.getstatusoutput(cmd)
+    (status,rel)=commands.getstatusoutput(cmd)
+    print(rel)
+    return  'it.s ok'
+
 #    return os.system(cmd)
 
 @itchat.msg_register(TEXT)
 def simple_reply(msg):
     msg=msg['Text']
+    global mode
     if msg in 'cmd':
         mode='cmd'
         return
     elif msg in 'auto':
         mode='auto'
         return
-    else :
+    elif msg in 'normal':
         mode='normal'
         return
-
+    print(mode)
     if mode in 'cmd':
-        return mode_cmd(msg['Text'])
+	print(msg)
+        if msg in 'wakeonlan':
+            mode_cmd('wakeonlan 40:8d:5c:bc:52:7d')
+            return 'wakeonlan'          
+        if msg in 'off':
+	    mode_cmd('echo . | sudo -S pm-suspend')
+	    return 'Computer is off' 
+
+        return mode_cmd(msg)
 
 itchat.auto_login(hotReload=True)
 itchat.run()
